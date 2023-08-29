247bdecc1165d30a2169f69ea58a6ac4328fcb5e
Bohdan <shcherbabohdan@gmail.com>
2023-08-29 12:49:52 +0300

    setuped tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './assets/css/App.css';
-import {useAppDispatch} from "./store/store";
-import {signIn} from "./store/app/action";
+import house_img from './assets/img/house.jpeg'
 
 function App() {
 
-  const dispatch = useAppDispatch()
-
-  dispatch(signIn('tokenqwe'))
-
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="w-full h-screen " >
+            <img src={house_img} alt="" className='w-full h-full object-cover filter blur-md'/>
+            <div className='absolute w-full h-full flex items-center justify-center top-0'>
+                <div className="w-[20%] h-[90%] bg-white rounded-xl mr-6 max-lg:hidden">
+                    q
+                </div>
+                <div className="w-[50%] h-[90%] bg-white rounded-xl max-lg:w-[85%]">
+                    q
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
